5076a685c0b0f114afe61a000be7b442f0941dba
JanSuran <suran.orgpad@gmail.com>
2021-09-18 00:25:44 +0200

    Removed lein-tornado plugin dependency.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject org.clojars.jansuran03/tornado "0.1.0"
+(defproject org.clojars.jansuran03/tornado "0.1.1"
   :description "A Clojure library similar to garden with focus on simplicity and comfort of the programmer. Still very
                 new, but I improve it as my time allows me to."
   :url "https://github.com/JanSuran03/tornado"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
-  :repl-options {:init-ns tornado.core}
-  :plugins [[org.clojars.jansuran03/lein-tornado "0.1.0-SNAPSHOT"]])
+  :repl-options {:init-ns tornado.core})
