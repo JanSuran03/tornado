2d7a62b41c444daf23ff65a642f288c9bcee7df2
Georgios Makris <makris.giwrgos1234@gmail.com>
2023-09-02 21:46:23 +0200

    Add `compression` unit tests
    Rudimentary, more complex cases might need to be added later.

---
+++ test/tornado/test/compression.clj
@@ -0,0 +1,18 @@
+(ns tornado.test.compression
+  (:require [clojure.test :refer :all]
+            [tornado.compression :as compression]))
+
+(deftest compress-whitespace
+  (is (= " a b c "
+         (compression/compress-whitespace"   a   \n b c   "))))
+
+(deftest strip-whitespace-around
+  (is (= "[]{};,:()"
+         (compression/strip-whitespace-around " [ ] { } ; , : ( ) "))))
+
+(deftest compress
+  (is (= "div{border-color:rgb(255,255,255);}"
+       (compression/compress
+           "div {
+                border-color: rgb( 255, 255, 255);
+            }"))))
