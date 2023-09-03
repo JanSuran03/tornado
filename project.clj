6b7a87f505e8571e8bbcc0f601236789fe36cad2
Jan Šuráň <jan.suran@second-foundation.eu>
2023-09-04 01:47:44 +0200

    Add :dev and :test Leiningen profiles.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :repl-options {:init-ns tornado.core}
-  :main tornado.test.core)
+  :profiles {:dev  {:repl-options {:init-ns tornado.core}}
+             :test {:repl-options {:init-ns tornado.test.core}}})
